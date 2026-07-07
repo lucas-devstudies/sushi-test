34af45996517a57256a11d3c7452150ab5bb41a6
lucas-devstudies <lucasfdasilva2002@gmail.com>
2026-07-07 08:09:51 -0400

    inserindo tela de login e criando novas rotas

--- Sushi3D/src/App.tsx
+++ Sushi3D/src/App.tsx
@@ -1,55 +1,18 @@
-import { useEffect, useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import { supabase } from '../supabase/supabaseClient'
-
-
-interface User {
-  id: number;
-  name: string;
-  email: string;
-}
+import DashRestaurant from './pages/dash_restaurant'
+import Login from './pages/login'
 
 function App() {
-
-  const [user,setUser] = useState<User | null>(null)
-  const [users,setUsers] = useState<User[]>([])
-  console.log(users)
-
-  //Ainda tô deixei as rls desativadas, mas preciso aprender a trabalhar com elas ligadas
-  async function fetchUsers(){
-    const {data,error} = await supabase
-    .from('users')
-    .select('*')
-  
-    if(error){
-      console.error('Erro ao buscar usuários:', error)
-    }
-    if(data){
-      setUsers(data)
-    }
-  }
-  async function createUser(name: string, email: string){
-    await supabase
-    .from('users')
-    .insert({name:user?.name,email:user?.email})
-  }
-
-  useEffect(()=>{
-    fetchUsers()
-  },[])
-
-  console.log(users)
-
   return (
     <>
-      <section id="center">
-        <h1>Lista de Usuários</h1>
-        <ul>
-          {users.map((user: User) => (
-            <li key={user.id}>{user.name} - {user.email}</li>
-          ))}
-        </ul>
-      </section>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<DashRestaurant />} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/restau" element={<DashRestaurant />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ Sushi3D/src/pages/dash_restaurant.tsx
@@ -0,0 +1,12 @@
+
+
+export default function DashRestaurant() {
+
+    return(
+        <>
+            <section id="center">
+                <h1>Dash Restaurant</h1>
+            </section>
+        </>
+    )
+}
