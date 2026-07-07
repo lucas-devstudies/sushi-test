31cbbe65d248cd031a3197760f157d3823b26c5a
lucas-devstudies <lucasfdasilva2002@gmail.com>
2026-07-07 09:44:08 -0400

    realizando teste de cadsatro de usuário, mas ainda sem validar por conta de erros no supabase

--- Sushi3D/src/pages/register.tsx
+++ Sushi3D/src/pages/register.tsx
@@ -1,18 +1,55 @@
+import { useState } from "react";
+import { supabase } from "../../supabase/supabaseClient";
 
 export default function Register() {
 
+    let [name, setName] = useState('');
+    let [email, setEmail] = useState('');
+    let [phone, setPhone] = useState('');
+    let [password, setPassword] = useState('');
+
+    async function signUp(){
+        const {data, error:authError} = await supabase.auth.signUp({
+            email: email,
+            password: password
+        })
+        if(authError){
+            console.log(authError);
+            return;
+        }
+        const usuarioId = data?.user?.id;
+        
+        if(usuarioId){
+            const {error: dbError} = await supabase
+            .from('users')
+            .insert([
+                {
+                    id: usuarioId,
+                    name: name,
+                    phone: phone,
+                }
+            ]);
+
+            if(dbError){
+                console.log(dbError);
+                return;
+            }
+        }
+        console.log('Cadastro realizado com sucesso!')
+    }
+
     return(
          <section id="login" className="flex justify-center items-center h-screen">
-            <form className="flex flex-col gap-5 shadow-[0_0_50px_-3px] shadow-blue-950 py-4 px-5 rounded-2xl">
+            <form onSubmit={(e)=> {e.preventDefault();signUp();}} className="flex flex-col gap-5 shadow-[0_0_50px_-3px] shadow-blue-950 py-4 px-5 rounded-2xl">
                 <h2 className="font-bold text-3xl">Register</h2>
                 <label className="w-100 text-start text-xl font-bold">Nome:</label>
-                <input className="border border-blue-950 text-xl px-2 py-2 rounded-xl placeholder:text-gray-500" type="text" name="name" placeholder="Digite seu nome"/>
-                <label className="w-100 text-start text-xl font-bold">CPF:</label>
-                <input className="border border-blue-950 text-xl px-2 py-2 rounded-xl placeholder:text-gray-500" type="text" name="cpf" placeholder="Digite seu CPF"/>
+                <input className="border border-blue-950 text-xl px-2 py-2 rounded-xl placeholder:text-gray-500" type="text" onChange={(e) => setName(e.target.value)} name="name" placeholder="Digite seu nome"/>
+                <label className="w-100 text-start text-xl font-bold">Telefone:</label>
+                <input className="border border-blue-950 text-xl px-2 py-2 rounded-xl placeholder:text-gray-500" type="text" onChange={(e) => setPhone(e.target.value)} name="phone" placeholder="Digite seu telefone"/>
                 <label className="w-100 text-start text-xl font-bold">Email:</label>
-                <input className="border border-blue-950 text-xl px-2 py-2 rounded-xl placeholder:text-gray-500" type="email" name="email" placeholder="Digite seu email"/>
+                <input className="border border-blue-950 text-xl px-2 py-2 rounded-xl placeholder:text-gray-500" type="email" onChange={(e) => setEmail(e.target.value)} name="email" placeholder="Digite seu email"/>
                 <label className="w-100 text-start text-xl font-bold">Senha:</label>
-                <input className="border border-blue-950 text-xl px-2 py-2 rounded-xl placeholder:text-gray-500" type="password" name="password" placeholder="Digite sua senha"/>
+                <input className="border border-blue-950 text-xl px-2 py-2 rounded-xl placeholder:text-gray-500" type="password" onChange={(e) => setPassword(e.target.value)} name="password" placeholder="Digite sua senha"/>
                 <button type="submit" className="text-xl text-center bg-blue-950 text-white font-bold py-2 rounded-xl">Criar Conta</button>
                 <a href="/login" className="text-xl text-center mb-4">Já tem uma conta? Faça login</a>
             </form>
