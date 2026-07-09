d0f1b765a31047d72f9dd74f4e643fd64180d1be
lucas-devstudies <lucasfdasilva2002@gmail.com>
2026-07-09 11:38:31 -0400

    inserindo nova fonte e atualizando animações

--- Sushi3D/src/components/CustomCard.tsx
+++ Sushi3D/src/components/CustomCard.tsx
@@ -10,9 +10,9 @@ export function CustomCard({id,title,image}:CustomCardProps){
         <div className="flex flex-col items-center">
 
             {id%2!==0 ?(
-                <img src={image} height={20} width={'80%'}></img>
+                <img src={image} height={20} width={'80%'} className="duration-300 hover:rotate-15"></img>
             ):(
-                <img src={image} height={20} width={'90%'}></img>
+                <img src={image} height={20} width={'90%'} className="duration-300 hover:rotate-15"></img>
             )}
             <h1 className="text-white text-3xl my-4">{title}</h1>
         </div>
--- Sushi3D/src/pages/dash_restaurant.tsx
+++ Sushi3D/src/pages/dash_restaurant.tsx
@@ -35,29 +35,32 @@ export default function DashRestaurant() {
 
     return(
         <main className="overflow-x-hidden">
-            <section className="w-screen h-screen">
-                <video src={videoBg} autoPlay loop muted style={{ width: '100dvw', height: '100dvh', objectFit: 'cover', zIndex: -1}}/>
+            <section className="w-screen h-screen overflow-hidden">
+                <video src={videoBg} autoPlay loop muted style={{margin:0, width: '100dvw', height: '100dvh', objectFit: 'cover', zIndex: -1}}/>
                 <button 
                     className='absolute left-1/2 bg-transparent -translate-x-1/2 z-20 bottom-5 flex items-center justify-center border-gray-400 border-2 hover:border-red-700 hover:scale-120' onClick={() => clicado()} style={styles.bt}>
                     <img src={icDown} alt="Seta para baixo" className="w-full h-full object-contain"/>
                 </button>
                 
                 <div className="flex flex-col h-screen justify-center items-start px-10 overflow-hidden" style={{position:'absolute',right:0,left:0,top:'50%',transform: 'translateY(-50%)',background: 'linear-gradient(270deg, rgba(217, 217, 217, 0.00) 0%, #000 100%)'}}>
-                    <h1 className="font-bold text-9xl text-white">Sushi<span className='text-red-700'>3D</span></h1>
+                    <h1 className="font-protest font-bold text-9xl text-white">SUSHI<span className='text-red-700'>3D</span></h1>
                     <p className="text-xl text-white italic">A experiência gastronômica e tecnológica do mundo</p>
                     <h2 className='text-white text-2xl my-4'>Faça sua reserva agora mesmo</h2>
                     <button className="bg-red-700 px-4 py-3 rounded-4xl text-white font-semibold text-xl hover:bg-red-800 active:scale-90" onClick={()=>clicado()}>Reservar agora</button>
                 </div>
             </section>
-            <section className="w-screen h-dvh">
-                <h2 className='text-5xl py-6 font-bold text-red-700'>PRATOS PRINCIPAIS</h2>
-                <div className="flex items-end justify-center mt-5">
+            <section className="w-screen">
+                <h2 className='text-5xl py-6 font-bold text-white font-protest'>Pratos Principais</h2>
+                <div className="flex items-end justify-center mt-5 mb-5">
                     {
                         listFoods.map((food) => (
-                            <CustomCard title={food.title} image={food.image} id={food.id}></CustomCard>
+                            <CustomCard title={food.title} image={food.image} id={food.id} key={food.id}></CustomCard>
                         ))
                     }
                 </div>
+            </section>
+            <section className='w-dvw h-dvh bg-black'>
+                <p>aaa</p>
             </section>
             <form className='w-screen overflow-hidden'>
                 <label htmlFor="">Teste de coisas</label>
