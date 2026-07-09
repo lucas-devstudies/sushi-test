b22524bf6efe6fb2b7e78587d48a026f83d72c6b
lucas-devstudies <lucasfdasilva2002@gmail.com>
2026-07-09 08:35:53 -0400

    inserindo botão para avançar na tela e corrindo erros visuais

--- Sushi3D/src/pages/dash_restaurant.tsx
+++ Sushi3D/src/pages/dash_restaurant.tsx
@@ -2,13 +2,21 @@ import videoBg from '../assets/video/videoBG.mp4'
 
 export default function DashRestaurant() {
 
+    function clicado(){
+        alert("desci")
+    }
+
     return(
-        <>
-            <video src={videoBg} autoPlay loop muted  style={{ width: '100vw', height: '100vh', objectFit: 'cover', position: 'fixed', top: 0, left: 0, zIndex: -1 }}/>
-            <section className="w-screen h-screen flex flex-col justify-center items-start px-10">
+        <main className="overflow-x-hidden">
+            <video src={videoBg} autoPlay loop muted  style={{ width: '100dvw', height: '100dvh', objectFit: 'cover', position: 'sticky', top: 0, left: 0, zIndex: -1}}/>
+            <button className='absolute left-0 right-0 z-1 tex-white text-center bottom-5' onClick={()=>clicado()} >Baixar</button>
+            <section className="flex flex-col justify-center items-start px-10 overflow-hidden" style={{position:'absolute',top:'50%',left:0}}>
                 <h1 className="font-bold text-8xl text-white">Sushi 3D</h1>
                 <p className="text-xl text-white">A experiência gastronômica e tecnológica do mundo</p>
             </section>
-        </>
+            <form className='w-screen overflow-hidden'>
+                <label htmlFor="">Teste de coisas</label>
+            </form>
+        </main>   
     )
 }
